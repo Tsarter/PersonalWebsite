b0a7fd095ab630cec5d6255d1376e82cda7e96a3
simplytobo <publictopia@gmail.com>
2022-09-28 23:19:09 +0300

    fixed infinite loop

--- src/components/projectsSlider.vue
+++ src/components/projectsSlider.vue
@@ -2,18 +2,104 @@
 
 
 <template>
-    <div class="Container" @click="slider()">
-        <ul class="list">
-            <li class="hide"></li>
-            <li class="prev"></li>
-            <li class="act"></li>
-            <li class="next"></li>
-            <li class="next new-next"></li>
+    <div class="CarouselContainer">
+        <ul class="Carousellist">
+            <li class="Carouselhide" id="0">1</li>
+            <li class="Carouselprev" id="1">2</li>
+            <li class="Carouselact" id="2">3</li>
+            <li class="Carouselnext" id="3">4</li>
+            <li class="Carouselnext Carouselnew-next" id="4">5</li>
         </ul>
-        <div class="swipe"></div>
+        <div class="Carouselswipe"></div>
     </div>
 </template>
 
+<style>
+
+
+    .CarouselContainer{
+        display: flex;
+        position: relative;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-content: center;
+        column-gap: 6rem;
+        padding: 1rem;
+        padding-top: 10rem;
+        padding-bottom: 15rem;
+        height:50vh;
+        width: 100%;
+        background-color: var(--main-color);
+    }
+    * {
+        padding: 0;
+        margin: 0;
+    }
+    .Carousellist {
+      height: 200px;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%,-50%)
+    }
+    
+    .Carousellist li {
+      list-style-type: none;
+      
+      width:30vw;
+      height: 40vh;
+      opacity: .25;
+      position: absolute;
+      left: 50%;
+      margin-left: -100px;
+      border-radius: 2px;
+      background: #9C89B8;
+      transition: transform 1s, opacity 1s;
+    }
+    
+    .Carousellist .Carouselact {
+      opacity: 1;
+    }
+    
+    .Carousellist .Carouselprev,
+    .Carousellist .Carouselnext {
+      cursor: pointer;
+    }
+    
+    .Carousellist .Carouselprev {
+      transform: translateX(-220px) scale(.85);
+    }
+    
+    .Carousellist .Carouselnext {
+      transform: translateX(220px) scale(.85);
+    }
+    
+    .Carousellist .Carouselhide {
+      transform: translateX(-420px) scale(.85);
+    }
+    
+    .Carousellist .Carouselnew-next {
+      transform: translateX(420px) scale(.85);
+    }
+    
+    .Carousellist .Carouselhide,
+    .Carousellist .Carouselnew-next {
+      opacity: 0;
+      transition: opacity .5s, transform .5s;
+    }
+    
+    .Carouselswipe {
+      width: 270px;
+      height: 200px;
+      position: absolute;
+      background-color: green;
+      border-radius: 2px;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%,-50%);
+      opacity: 0;
+    }
+</style>
 <script>
  export default {
     mounted(){
@@ -21,77 +107,88 @@
     },
     methods:{
         slider(){
+            var Cards = {}
+            var CardIdToPushRight = 0
+            var CardIdToPushLeft = 0
             const $ = selector => {
             return document.querySelector(selector);
             };
 
             function next() {
-                if ($(".hide")) {
-                    $(".hide").remove(); 
+                if ($(".Carouselhide")) {
+                    CardIdToPushRight = parseInt($(".Carouselhide").id)
+                    /* Add card to memory/dict, then remove it */
+                    Cards[$(".Carouselhide").id] = $(".Carouselhide")
+                    $(".Carouselhide").remove();             
                 }
 
                 /* Step */
 
-                if ($(".prev")) {
-                    $(".prev").classList.add("hide");
-                    $(".prev").classList.remove("prev");
+                if ($(".Carouselprev")) {
+                    $(".Carouselprev").classList.add("Carouselhide");
+                    $(".Carouselprev").classList.remove("Carouselprev");
                 }
 
-                $(".act").classList.add("prev");
-                $(".act").classList.remove("act");
+                $(".Carouselact").classList.add("Carouselprev");
+                $(".Carouselact").classList.remove("Carouselact");
 
-                $(".next").classList.add("act");
-                $(".next").classList.remove("next");
+                $(".Carouselnext").classList.add("Carouselact");
+                $(".Carouselnext").classList.remove("Carouselnext");
 
                 /* New Next */
+                console.log($(".Carouselnew-next"))
+                $(".Carouselnew-next").classList.remove("Carouselnew-next");
 
-                $(".new-next").classList.remove("new-next");
+                /* Append card to list */
+                Cards[CardIdToPushRight].classList.remove("Carouselhide")
+                Cards[CardIdToPushRight].classList.add("Carouselnext","Carouselnew-next");
+                $(".Carousellist").appendChild(Cards[CardIdToPushRight]);
 
-                const addedEl = document.createElement('li');
-
-                $(".list").appendChild(addedEl);
-                addedEl.classList.add("next","new-next");
             }
 
             function prev() {
-            $(".new-next").remove();
+            CardIdToPushLeft = parseInt($(".Carouselnew-next").id)
+            /* Add card to memory/dict, then remove it */
+            Cards[$(".Carouselnew-next").id] = $(".Carouselnew-next")
+            $(".Carouselnew-next").remove();
                 
             /* Step */
 
-            $(".next").classList.add("new-next");
+            $(".Carouselnext").classList.add("Carouselnew-next");
 
-            $(".act").classList.add("next");
-            $(".act").classList.remove("act");
+            $(".Carouselact").classList.add("Carouselnext");
+            $(".Carouselact").classList.remove("Carouselact");
 
-            $(".prev").classList.add("act");
-            $(".prev").classList.remove("prev");
+            $(".Carouselprev").classList.add("Carouselact");
+            $(".Carouselprev").classList.remove("Carouselprev");
 
             /* New Prev */
 
-            $(".hide").classList.add("prev");
-            $(".hide").classList.remove("hide");
+            $(".Carouselhide").classList.add("Carouselprev");
+            $(".Carouselhide").classList.remove("Carouselhide");
 
-            const addedEl = document.createElement('li');
-
-            $(".list").insertBefore(addedEl, $(".list").firstChild);
-            addedEl.classList.add("hide");
+            const addedEl = Cards[CardIdToPushLeft]
+            addedEl.classList.remove("Carouselnew-next")
+            addedEl.classList.remove("Carouselnext")
+            $(".Carousellist").insertBefore(addedEl, $(".Carousellist").firstChild);
+            addedEl.classList.add("Carouselhide");
             }
 
             var slide = element => {
             /* Next slide */
             
-                if (element.classList.contains('next')) {
+                if (element.classList.contains('Carouselnext')) {
                     next();
                     
                 /* Previous slide */
                     
-                } else if (element.classList.contains('prev')) {
+                } else if (element.classList.contains('Carouselprev')) {
                     prev();
                 }
             }
 
-            const slider = $(".list"),
-                swipe = new Hammer($(".swipe"));
+            const slider = $(".Carousellist"),
+                swipe = new Hammer($(".Carouselswipe"));
 
             slider.onclick = event => {
             slide(event.target);
@@ -109,89 +206,3 @@
  }
 </script>
 
-<style>
-
-
-.Container{
-    display: flex;
-    position: relative;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-content: center;
-    column-gap: 6rem;
-    padding: 1rem;
-    padding-top: 10rem;
-    padding-bottom: 15rem;
-    height:80vh;
-    width: 100%;
-    background-color: var(--main-color);
-}
-* {
-    padding: 0;
-    margin: 0;
-}
-.list {
-  height: 200px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%,-50%)
-}
-
-.list li {
-  list-style-type: none;
-  
-  width: 200px;
-  height: 200px;
-  opacity: .25;
-  position: absolute;
-  left: 50%;
-  margin-left: -100px;
-  border-radius: 2px;
-  background: #9C89B8;
-  transition: transform 1s, opacity 1s;
-}
-
-.list .act {
-  opacity: 1;
-}
-
-.list .prev,
-.list .next {
-  cursor: pointer;
-}
-
-.list .prev {
-  transform: translateX(-220px) scale(.85);
-}
-
-.list .next {
-  transform: translateX(220px) scale(.85);
-}
-
-.list .hide {
-  transform: translateX(-420px) scale(.85);
-}
-
-.list .new-next {
-  transform: translateX(420px) scale(.85);
-}
-
-.list .hide,
-.list .new-next {
-  opacity: 0;
-  transition: opacity .5s, transform .5s;
-}
-
-.swipe {
-  width: 270px;
-  height: 200px;
-  position: absolute;
-  background-color: green;
-  border-radius: 2px;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%,-50%);
-  opacity: 0;
-}
-</style>
